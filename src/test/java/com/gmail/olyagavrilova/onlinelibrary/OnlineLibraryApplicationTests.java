1b78905cf5475a530ab6d37cccebfc5aa8a8e0b2
Olga Gavrilova <olyagavrilova92@gmail.com>
2020-09-24 19:32:42 +0300

    Initial Commit

---
+++ src/test/java/com/gmail/olyagavrilova/onlinelibrary/OnlineLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.olyagavrilova.onlinelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
